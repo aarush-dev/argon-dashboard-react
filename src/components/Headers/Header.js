8fe41d541feafc52fd379c17a4d2e6a122e72e77
aarush-dev <aarushmahajan.dev@gmail.com>
2022-10-10 09:14:12 +0530

    commit

--- src/components/Headers/Header.js
+++ src/components/Headers/Header.js
@@ -6,7 +6,12 @@
 
 * Product Page: https://www.creative-tim.com/product/argon-dashboard-react
 * Copyright 2022 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-react/blob/master/LICENSE.md)
+* Licensed under MIT (https://github.com/creativetimofficial/
+
+
+
+
+-dashboard-react/blob/master/LICENSE.md)
 
 * Coded by Creative Tim
 
--- src/layouts/Auth.js
+++ src/layouts/Auth.js
@@ -64,13 +64,19 @@ const Auth = (props) => {
         <AuthNavbar />
         <div className="header bg-gradient-info py-7 py-lg-8">
           <Container>
-            <div className="header-body text-center mb-7">
+            <div className="header-body text-center mb-8">
               <Row className="justify-content-center">
                 <Col lg="5" md="6">
                   <h1 className="text-white">Welcome!</h1>
                   <p className="text-lead text-light">
-                    Use these awesome forms to login or create new account in
-                    your project for free.
+
+
+
+                  </p>
+                  <p className="text-lead text-light">
+
+
+
                   </p>
                 </Col>
               </Row>
